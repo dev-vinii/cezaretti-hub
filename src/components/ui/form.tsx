7740a4671d7d8ba42c3f2f2cf2bc9c571661d2a8
Vinicius <viniciusluna@yahoo.com>
2024-08-26 09:03:37 -0300

    feat(ui): add ui components

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -156,7 +156,10 @@ const FormMessage = React.forwardRef<
     <p
       ref={ref}
       id={formMessageId}
-      className={cn("text-destructive text-sm font-medium", className)}
+      className={cn(
+        "text-destructive text-sm font-medium text-red-600",
+        className,
+      )}
       {...props}
     >
       {body}
