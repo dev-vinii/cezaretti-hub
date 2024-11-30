111e243ce1a21e4f11a010b1c4015d69759f01d9
Vinicius Luna <viniciusluna@yahoo.com>
2024-11-30 19:35:00 -0300

    feat(sidebar): selected page indicator

--- src/components/widgets/organisms/sidebar.tsx
+++ src/components/widgets/organisms/sidebar.tsx
@@ -1,15 +1,32 @@
 import { LogOut, Package, User } from "lucide-react";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const sidebarItems = [
+  { path: "/", icon: <User />, label: "Home" },
+  { path: "/products", icon: <Package />, label: "Products" },
+];
 
 function SideBar() {
-  function navigateTo(path: string) {
-    window.location.href = path;
-  }
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   return (
     <aside className="flex h-screen w-20 flex-col items-center justify-between bg-[#040404] p-4 text-white">
       <div className="flex cursor-pointer flex-col items-center gap-4">
-        <User onClick={() => navigateTo("/")} />{" "}
-        <Package onClick={() => navigateTo("/products")} />
+        {sidebarItems.map(({ path, icon }) => (
+          <div
+            key={path}
+            onClick={() => navigate(path)}
+            className="flex flex-col items-center text-white"
+          >
+            {icon}
+            <span
+              className={`mt-1 h-1 w-6 rounded-full ${
+                pathname === path ? "bg-white" : "bg-transparent"
+              }`}
+            />
+          </div>
+        ))}
       </div>
       <LogOut />
     </aside>
